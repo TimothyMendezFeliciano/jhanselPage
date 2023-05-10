311fd4147374a577c0407a6ab3cdd39b8d8d6f86
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2023-05-09 20:38:08 -0400

    Images Rearranged

--- src/pages/api/ContactFormImages.ts
+++ src/pages/api/ContactFormImages.ts
@@ -1,12 +1,25 @@
-import jhanselOne from '/public/images/jhanselProfile2/IMG_3562.jpg'
-import jhanselTwo from '/public/images/jhanselProfile2/IMG_3573.jpg'
+// import jhanselOne from '/public/images/jhanselProfile2/IMG_3562.jpg'
+// import jhanselTwo from '/public/images/jhanselProfile2/IMG_3573.jpg'
+// import jhanselFive from '/public/images/jhanselProfile1/five.jpg'
+import jhanselSix from '/public/images/jhanselProfile1/six.jpg'
+import jhanselFour from '/public/images/jhanselProfile1/four.jpg'
+
 const Images = [
+    // {
+    //     original: jhanselOne.src,
+    // },
+    // {
+    //     original: jhanselTwo.src,
+    // }
+    // {
+    //     original: jhanselFive.src,
+    // },
     {
-        original: jhanselOne.src,
+        original: jhanselSix.src,
     },
     {
-        original: jhanselTwo.src,
-    }
+        original: jhanselFour.src,
+    },
 ]
 
 export default Images
--- src/pages/api/HeroImages.ts
+++ src/pages/api/HeroImages.ts
@@ -1,9 +1,7 @@
 import jhanselOne from '/public/images/jhanselProfile1/one.jpg'
 import jhanselTwo from '/public/images/jhanselProfile1/two.jpg'
 import jhanselThree from '/public/images/jhanselProfile1/three.jpg'
-import jhanselFour from '/public/images/jhanselProfile1/four.jpg'
-import jhanselFive from '/public/images/jhanselProfile1/five.jpg'
-import jhanselSix from '/public/images/jhanselProfile1/six.jpg'
+
 
 const HeroImages = [
     {
@@ -15,16 +13,6 @@ const HeroImages = [
     {
         original: jhanselThree.src,
     },
-    {
-        original: jhanselFour.src,
-    },
-    {
-        original: jhanselFive.src,
-    },
-    {
-        original: jhanselSix.src,
-    },
-
 ]
 
 export default HeroImages
